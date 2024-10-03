cf32e27d55e9cc399cdad9f73d39db2a5b9620b8
Sriharsha171 <bksh178@gmail.com>
2024-10-03 12:06:40 -0700

    Delete Ai form filler advanced directory

--- Ai form filler advanced/image2pdf.py
+++
@@ -1,5 +0,0 @@
-def image_to_pdf(image_path, output_pdf_path):
-    image = Image.open(image_path)
-    pdf_bytes = image.convert('RGB')
-    pdf_bytes.save(output_pdf_path, "PDF", resolution=100.0)
-    print(f"PDF saved: {output_pdf_path}")
